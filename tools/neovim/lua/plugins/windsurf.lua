7ce853978948df620b927294f56edfc18e48306c
Micha Pietsch <michapietsch@mail.de>
2025-09-19 11:23:45 +0200

    Put Windsurf autocomplete to Option+Tab, accept word by word with Shift+Tab

--- tools/neovim/lua/plugins/windsurf.lua
+++ tools/neovim/lua/plugins/windsurf.lua
@@ -1,6 +1,24 @@
 return {
-  {
-    "Exafunction/windsurf.vim",
-    event = "BufEnter",
+  "Exafunction/windsurf.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
   },
+  config = function()
+    require("codeium").setup({
+      enable_cmp_source = false,
+      virtual_text = {
+        enabled = true,
+        manual = false, -- No auto-completions
+        key_bindings = {
+          accept = "<M-Tab>", -- Disable Tab for full completion
+          accept_word = "<S-Tab>", -- Tab accepts next word only
+        },
+      },
+    })
+
+    vim.keymap.set("i", "<C-Space>", function()
+      require("codeium.virtual_text").cycle_or_complete()
+    end)
+  end,
 }
